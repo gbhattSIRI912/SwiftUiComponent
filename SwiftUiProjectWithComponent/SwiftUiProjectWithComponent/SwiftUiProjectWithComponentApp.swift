74ff51ad3efc70a118cd3c6a94a6c85a64dd630b
bhatt.gaurav <gauravbhatt913@gmail.com>
2025-11-16 09:41:49 +0530

    Initial SwiftUI project created

---
+++ SwiftUiProjectWithComponent/SwiftUiProjectWithComponent/SwiftUiProjectWithComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiProjectWithComponentApp.swift
+//  SwiftUiProjectWithComponent
+//
+//  Created by Gaurav Bhatt on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiProjectWithComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
