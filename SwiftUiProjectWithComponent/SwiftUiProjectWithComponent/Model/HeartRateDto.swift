82b85b611e10fd7df9c1c2def3031a73c9766f0e
bhatt.gaurav <gauravbhatt913@gmail.com>
2025-11-30 11:29:46 +0530

    Add files related to dto and view

---
+++ SwiftUiProjectWithComponent/SwiftUiProjectWithComponent/Model/HeartRateDto.swift
@@ -0,0 +1,8 @@
+//
+//  HeartRateDto.swift
+//  SwiftUiProjectWithComponent
+//
+//  Created by Gaurav Bhatt on 18/11/25.
+//
+
+import Foundation
---
+++ SwiftUiProjectWithComponent/SwiftUiProjectWithComponent/Views/HeartRateView/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  SwiftUiProjectWithComponent
+//
+//  Created by Gaurav Bhatt on 18/11/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
